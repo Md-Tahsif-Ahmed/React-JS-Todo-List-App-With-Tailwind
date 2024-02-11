060be88809672943f923d362541afe2c8557296d
tahsif <tahsif.cse@gmail.com>
2024-02-11 19:33:49 +0600

    Worked on Priority

--- src/Pages/Components/TaskItem.jsx
+++ src/Pages/Components/TaskItem.jsx
@@ -7,12 +7,29 @@ const TaskItem = ({ task, toggleTaskStatus }) => {
   const handleToggleStatus = () => {
     toggleTaskStatus(task.id);
   };
+
+  const priorityColors = {
+    low: 'green',
+    medium: 'orange',
+    high: 'red'
+  };
+
+  const priorityStyles = {
+    borderRadius: '50%',
+    width: '12px',
+    height: '12px',
+    backgroundColor: priorityColors[task.priority],
+    display: 'inline-block',
+    marginRight: '8px'
+  };
  
  return (  
       <div className="space-x-6 space-y-3 ">
-      <span style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>{task.title} </span>
-      <button onClick={handleToggleStatus} className="btn btn-success btn-xs">
-          {task.completed ? 'Incomplete' : 'Complete'}
+        <span style={priorityStyles}></span>
+        <span>{task.priority} </span>
+        <span style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>{task.title} </span>
+        <button onClick={handleToggleStatus} className="btn btn-success btn-xs">
+            {task.completed ? 'Incomplete' : 'Complete'}
         </button>
       </div>
       
