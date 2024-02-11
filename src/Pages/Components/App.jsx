ec9df17c95a2736ce11510a64267884a1682100f
tahsif <tahsif.cse@gmail.com>
2024-02-11 19:48:38 +0600

    Worked on Filter tasks

--- src/Pages/Components/App.jsx
+++ src/Pages/Components/App.jsx
@@ -5,6 +5,8 @@ const App = () => {
     const [tasks, setTasks] = useState([]);
     const [totalTasks, setTotalTasks] = useState(0);
     const [completedTasks, setCompletedTasks] = useState(0);
+    const [filteredTasks, setFilteredTasks] = useState([]);
+    const [filterPriority, setFilterPriority] = useState('all');
     // Local storage Work
     useEffect(() => {
         const storedTasks = JSON.parse(localStorage.getItem('tasks'));
@@ -16,8 +18,9 @@ const App = () => {
       useEffect(() => {
         localStorage.setItem('tasks', JSON.stringify(tasks));
         updateTaskCounters();
+        filterTasks();
        
-      }, [tasks]);
+      }, [tasks, filterPriority]);
     
     // Add Task
     const addTask = (title, priority) => {
@@ -38,17 +41,30 @@ const App = () => {
           return task;
         }));
       };
-
-      //Counter
-      const updateTaskCounters = () => {
-        setTotalTasks(tasks.length);
-        const completedTasksCount = tasks.filter(task => task.completed).length;
-        setCompletedTasks(completedTasksCount);
-    };
     // Delete
     const deleteTask = (taskId) => {
         setTasks(tasks.filter(task => task.id !== taskId));
       };
+
+    //Counter
+    const updateTaskCounters = () => {
+    setTotalTasks(tasks.length);
+    const completedTasksCount = tasks.filter(task => task.completed).length;
+    setCompletedTasks(completedTasksCount);
+    };
+    // Filter
+    const filterTasks = () => {
+        if (filterPriority === 'all') {
+          setFilteredTasks(tasks);
+        } else {
+          setFilteredTasks(tasks.filter(task => task.priority === filterPriority));
+        }
+      };
+    
+      const handlePriorityFilterChange = (e) => {
+        setFilterPriority(e.target.value);
+      };
+    
     
     return (
         <div className='space-y-4'>
@@ -58,8 +74,17 @@ const App = () => {
       <div><span className='font-bold'>Completed Tasks:</span> {completedTasks}</div>
       </div>
       <TaskForm onSubmit={addTask} />
+      <div className='text-center'>
+        <label>Filter by Priority: </label>
+        <select value={filterPriority} onChange={handlePriorityFilterChange} className="select select-bordered select-sm w-1/4 lg:w-full max-w-xs">
+            <option value="all">All</option>
+            <option value="low">Low</option>
+            <option value="medium">Medium</option>
+            <option value="high">High</option>
+        </select>
+      </div>
       <TaskList
-         tasks={tasks}
+         tasks={filteredTasks}
          toggleTaskStatus={toggleTaskStatus}
          deleteTask={deleteTask}
       />
