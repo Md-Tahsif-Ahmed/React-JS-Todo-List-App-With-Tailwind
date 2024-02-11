b3063ebb31456fe91dbf50dd08838ea1fc53d3ee
tahsif <tahsif.cse@gmail.com>
2024-02-11 19:09:25 +0600

    created Add Task funtionallity

--- src/Pages/Components/App.jsx
+++ src/Pages/Components/App.jsx
@@ -1,8 +1,26 @@
+import { useState } from 'react';
+import TaskForm from './TaskForm';
 const App = () => {
+    const [tasks, setTasks] = useState([]);
+    // Add Task
+    const addTask = (title, priority) => {
+        const newTask = {
+          id: tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1,
+          title,
+          priority,
+          completed: false,
+        };
+        setTasks([...tasks, newTask]);
+      };
     return (
-        <div>
-             
-        </div>
+        <div className='space-y-4'>
+      <h1 className='text-2xl text-center font-medium my-10'>Todo List App</h1>
+       
+       
+      <TaskForm onSubmit={addTask} />
+       
+      </div>
+    
     );
 };
 
