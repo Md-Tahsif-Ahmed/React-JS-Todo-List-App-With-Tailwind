99089c26c2ebedc4c879ffcff3b5d79a250fca29
tahsif <tahsif.cse@gmail.com>
2024-02-11 19:24:39 +0600

    Worked on Local Storage

--- src/Pages/Components/App.jsx
+++ src/Pages/Components/App.jsx
@@ -1,8 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import TaskForm from './TaskForm';
 import TaskList from './TaskList';
 const App = () => {
     const [tasks, setTasks] = useState([]);
+    // Local storage Work
+    useEffect(() => {
+        const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+        if (storedTasks && storedTasks.length > 0) {
+          setTasks(storedTasks);
+        }
+      }, []);
+    
+      useEffect(() => {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+         
+       
+      }, [tasks]);
+    
     // Add Task
     const addTask = (title, priority) => {
         const newTask = {
